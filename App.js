e90e1eafb24c0faf60eb5031d6e59a9e7b727829
Chelsey Wang <chelseyyywang@gmail.com>
2020-01-30 12:29:04 -0800

    set up pages

--- App.js
+++ App.js
@@ -28,48 +28,34 @@ import 'react-native-gesture-handler';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { enableScreens } from 'react-native-screens';
-import HomeComp from './src/screens/Home/Home'; 
-
-enableScreens();
-
-
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-      </View>
-    );
-  }
-}
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-      </View>
-    );
-  }
-}
+// screens
+import Home from './src/screens/Home/Home'; 
+import Login from './src/screens/Login/Login'; 
+import Profile from './src/screens/Profile/Profile'; 
+import Signup from './src/screens/Signup/Signup'; 
 
 const AppNavigator = createStackNavigator(
   {
     Home: {
-      screen: HomeComp, 
+      screen: Home, 
       navigationOptions: {
           header: null,
       },
     },
-    Details: {
-      screen: DetailsScreen, 
+    Login: {
+      screen: Login, 
+      navigationOptions: {
+          header: null,
+      },
+    },
+    Profile: {
+      screen: Profile, 
+      navigationOptions: {
+          header: null,
+      },
+    },
+    Signup: {
+      screen: Signup, 
       navigationOptions: {
           header: null,
       },
@@ -84,51 +70,6 @@ const AppNavigator = createStackNavigator(
     },
   }
 );
-
-const styles = StyleSheet.create({
-  partiic: {
-    fontWeight: 'bold',
-    fontSize: 30, 
-    marginTop: 30, 
-    marginLeft: 50, 
-  }, 
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default createAppContainer(AppNavigator);
 
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {StyleSheet, Image, Text} from 'react-native';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Button
+  } from 'react-native';
 
-
-export default class HomeComp extends Component {
+export default class Home extends Component {
     render() {
       return (
+          <View>
         <Text style={styles.partiic}> Welcome to Partiic</Text> 
+        <Button
+          title="Go to Login"
+          onPress={() => this.props.navigation.navigate('Login')}
+        />
+        <Button
+          title="Go to Profile"
+          onPress={() => this.props.navigation.navigate('Profile')}
+        />
+        <Button
+          title="Go to Signup"
+          onPress={() => this.props.navigation.navigate('Signup')}
+        />
+        </View>
       );
     }
   }
