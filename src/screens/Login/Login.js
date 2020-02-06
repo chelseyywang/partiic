c9d1dca313a8e82578752fa1f98a69b7d77c6944
Chelsey Wang <chelseyyywang@gmail.com>
2020-02-05 22:16:02 -0800

    login & signup

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -15,6 +15,10 @@ import {
   View,
   Text,
   StatusBar,
+  KeyboardAvoidingView, 
+  Button, 
+  TouchableOpacity, 
+  Linking
 } from 'react-native';
 
 import {
@@ -25,35 +29,82 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import InputField from '../../components/InputField'; 
+
 
 export default class Login extends React.Component {
   render() {
     return (
       <>
-      <StatusBar barStyle="dark-content" />
+      <KeyboardAvoidingView style={styles.avoidView} behavior="padding">
+      {/* <StatusBar barStyle="dark-content" /> */}
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-            <Text style={styles.sectionTitle}>LOGIN</Text>
-
+            <Text style={styles.sectionTitle}>PARTIIC</Text>
+            <InputField labelText=' Username: ' />
+            <InputField labelText=' Password: ' />
+            <TouchableOpacity 
+            style={styles.loginButton}>
+              <Text style={styles.loginText}>Log In</Text>
+            </TouchableOpacity>
+            <Text style={styles.need}>Need an account? 
+              {" "}
+              <Text
+                style={{textDecorationLine: 'underline'}}
+                onPress={() => this.props.navigation.navigate('Signup')}
+              >
+                Sign up
+              </Text>
+            </Text>
         </ScrollView>
       </SafeAreaView>
+      </KeyboardAvoidingView>
     </>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  avoidView: {
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 300,
+    flex:1, 
+    backgroundColor: "#050A39"
+   },
+  loginText: {
+    fontSize: 18, 
+    paddingTop: 12,
+    textAlign: "center"
+    // color: "blue"
+  },
+  loginButton: {
+    borderRadius: 11, 
+    marginTop: 10,
+    paddingTop: 1,
+    paddingBottom: 5, 
+    fontSize: 18, 
+    backgroundColor: 'white', 
+    height: 50, 
+    textAlign: "center",
+  },
+  need: {
+    color: "#75F6F2", 
+    fontSize: 14, 
+    textAlign: "center", 
+    paddingTop: 10
+  },
   partiic: {
     fontWeight: 'bold',
     fontSize: 30, 
     marginTop: 30, 
     marginLeft: 50, 
   }, 
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
+  // scrollView: {
+  //   backgroundColor: Colors.lighter,
+  // },
   engine: {
     position: 'absolute',
     right: 0,
@@ -68,7 +119,8 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
-    color: Colors.black,
+    color: "white",
+    textAlign: "center"
   },
   sectionDescription: {
     marginTop: 8,
